2d64a6b79aecae58bafbecc9ddbe2ff827042015
Mustafa <971530+tosbaha@users.noreply.github.com>
2023-01-04 17:32:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Appcircle _Firebase Deployment_ component
+
+Deploy your web applications to Firebase Hosting
+
+## Required Inputs
+
+- `AC_FIREBASE_VERSION`: Firebase Version. Firebase version to be used. Enter v11.11.0 for a specific version.
+- `AC_FIREBASE_WEB_PATH`: Path of the web application. Full path of the web application. Don't change if you're using Flutter Web Build.
+
+## Optional Inputs
+
+- `AC_FIREBASE_TOKEN`: Firebase Token. A refresh token that's printed when you authenticate with `firebase login:ci` command. **Either select Firebase token or Google Service account**.
+- `GOOGLE_APPLICATION_CREDENTIALS`: Google Service Account. Path of Google Service Account JSON. Upload service account as a file to your environment group and name it `GOOGLE_APPLICATION_CREDENTIALS`. **Either select Firebase token or Google Service account**.
+- `AC_FIREBASE_EXTRA_PARAMETERS`: Extra parameters. Extra command line parameters. Enter --debug for debug mode.
---
+++ main.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'English'
+require 'os'
+require 'pathname'
+require 'fileutils'
+
+def get_env_variable(key)
+  ENV[key].nil? || ENV[key] == '' ? nil : ENV[key]
+end
+
+def run_command(cmd)
+  puts "@@[command] #{cmd}"
+  output = `#{cmd}`
+  raise "Command failed. Check logs for details \n\n #{output}" unless $CHILD_STATUS.success?
+
+  output
+end
+
+def install_firebase(path)
+  version = get_env_variable('AC_FIREBASE_VERSION').nil? ? 'latest' : ENV['AC_FIREBASE_VERSION']
+  os = OS.mac? ? 'macos' : 'linux'
+  FileUtils.mkdir_p(path) unless File.directory?(path)
+  run_command("curl -L https://firebase.tools/bin/#{os}/#{version} -o #{path}/firebase")
+  run_command("chmod +rx #{path}/firebase")
+end
+
+web_path = get_env_variable('AC_FIREBASE_WEB_PATH')
+raise 'Web path is empty' if web_path.nil?
+
+token = get_env_variable('AC_FIREBASE_TOKEN')
+service_account = get_env_variable('GOOGLE_APPLICATION_CREDENTIALS')
+extra_parameters = get_env_variable('AC_FIREBASE_EXTRA_PARAMETERS')
+
+raise 'No token or service account provided.' if token.nil? && service_account.nil?
+
+raise 'Either use token or service account, not both.' if !token.nil? && !service_account.nil?
+
+ac_temp = get_env_variable('AC_TEMP_DIR') || abort('Missing AC_TEMP_DIR variable.')
+firebase_path = File.join(ac_temp, 'firebasetools')
+install_firebase(firebase_path)
+cmd = "cd \"#{web_path}\" && #{firebase_path}/firebase deploy"
+cmd += " --token \"#{token}\"" unless token.nil?
+cmd += " #{extra_parameters}" unless extra_parameters.nil?
+result = run_command(cmd)
+puts result
+exit 0
